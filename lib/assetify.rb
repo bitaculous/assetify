a8e9788af51d85c43f5cb1a4cd837ab0d995359d
Maik Kempe <mkempe@nrolla.com>
2013-09-09 23:23:44 +0200

    [➠] Shuffled things around.

--- lib/assetify.rb
+++ lib/assetify.rb
@@ -1,12 +1,14 @@
 require 'sass'
 
 module Assetify
-  assets = File.expand_path '../../assets', __FILE__
+  assets        = File.expand_path '../../assets', __FILE__
   vendor_assets = File.expand_path '../../vendor/assets', __FILE__
 
-  stylesheets = File.expand_path 'stylesheets', assets
+  stylesheets        = File.expand_path 'stylesheets', assets
   vendor_stylesheets = File.expand_path 'stylesheets', vendor_assets
 
-  Sass.load_paths << stylesheets
-  Sass.load_paths << vendor_stylesheets
+  load_paths = Sass.load_paths
+
+  load_paths << stylesheets
+  load_paths << vendor_stylesheets
 end

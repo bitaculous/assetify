ec40452e54688a9f6ce8df6d3611d649de31237d
Maik Kempe <mkempe@bitaculous.com>
2014-08-17 20:55:46 +0200

    [➠] Updated “velocity”.

--- vendor/assets/javascripts/jquery/plugins/velocity/ui/ui-4.1.1.js
+++ vendor/assets/javascripts/jquery/plugins/velocity/ui/ui-4.1.1.js
@@ -1,16 +1,8 @@
-/***************
-    Details
-***************/
+/**********************
+   Velocity UI Pack
+**********************/
 
-/*!
-* velocity.ui.js: UI effects pack for Velocity. Load this file after jquery.velocity.js.
-* @version 4.1.0
-* @docs http://velocityjs.org/#uiPack
-* @support <=IE8: Callouts will have no effect, and transitions will simply fade in/out. IE9/Android 2.3: Most effects are fully supported, the rest fade in/out. All other browsers: Full support.
-* @license Copyright Julian Shapiro. MIT License: http://en.wikipedia.org/wiki/MIT_License
-* @license Indicated portions adapted from Animate.css, copyright Daniel Eden. MIT License: http://en.wikipedia.org/wiki/MIT_License
-* @license Indicated portions adapted from Magic.css, copyright Christian Pucci. MIT License: http://en.wikipedia.org/wiki/MIT_License
-*/   
+/* VelocityJS.org UI Pack (4.1.1). (C) 2014 Julian Shapiro. MIT @license: en.wikipedia.org/wiki/MIT_License. Portions copyright Daniel Eden, Christian Pucci. */
 
 (function() {
 
@@ -21,7 +13,7 @@
     var Container = (window.jQuery || window.Zepto || window);
 
     if (!Container.Velocity || !Container.Velocity.Utilities) {
-        console.log("Velocity UI Pack: Velocity must be loaded first. Aborting.");
+        window.console && console.log("Velocity UI Pack: Velocity must be loaded first. Aborting.");
 
         return;
     }
@@ -34,7 +26,7 @@
     }
 
     /******************
-       Registration
+       Register UI
     ******************/
 
     Container.Velocity.RegisterUI = function (effectName, properties) {
@@ -83,6 +75,7 @@
                 opts.queue = sequenceOptions.queue || "";
                 opts.easing = callOptions.easing || "ease";
                 opts.delay = callOptions.delay || 0;
+                opts._cacheValues = callOptions._cacheValues || true;
 
                 /* Special processing for the first effect call. */
                 if (callIndex === 0) {
@@ -180,6 +173,7 @@
     *********************/
 
     /* Externalize the packagedEffects data so that they can optionally be modified and re-registered. */
+    /* Support: <=IE8: Callouts will have no effect, and transitions will simply fade in/out. IE9/Android 2.3: Most effects are fully supported, the rest fade in/out. All other browsers: full support. */
     Container.Velocity.RegisterUI.packagedEffects = 
         { 
             /* Animate.css */
